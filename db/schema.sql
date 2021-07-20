22be2bb148ea919acfe5965da63b0c2ff9568e31
zoomzoom6 <tonyhuang107@live.ca>
2021-07-20 15:30:48 -0400

    update SQL tables with foreign key references

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employees;
+
+
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     dept_name VARCHAR(30) NOT NULL
@@ -7,13 +12,16 @@ CREATE TABLE roles (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INTEGER NOT NULL
+    department_id INTEGER,
+    CONSTRAINT fk_dept FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
 
 CREATE TABLE employees (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER NOT NULL,
-    manager_id INTEGER
+    role_id INTEGER,
+    manager_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
 )
