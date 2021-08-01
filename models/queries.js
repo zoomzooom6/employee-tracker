b016efccde9b8405fb4c36f6a1e06d9e0c7f4885
zoomzoom6 <tonyhuang107@live.ca>
2021-08-01 14:45:55 -0400

    tried out switch statement to continue program after creation of department or role

--- models/queries.js
+++ models/queries.js
@@ -1,10 +1,9 @@
 const db = require('../db/connection');
 const cTable = require('console.table');
 
-const getUsers = (id) => {
+const getAllEmp = () => {
     db.query(
-        'SELECT e.id, e.first_name, e.last_name, m.first_name AS manager FROM employees e LEFT JOIN employees m ON e.manager_id = m.id WHERE e.id = ?', 
-        [id],
+        'SELECT e.id, e.first_name, e.last_name, r.title AS job_title, d.dept_name AS department, r.salary AS salary, m.first_name AS manager FROM employees e LEFT JOIN roles r ON e.role_id = r.id LEFT JOIN department d ON r.department_id = d.id LEFT JOIN employees m ON e.manager_id = m.id', 
         function(err, result) {
             if(err) {
                 console.log(err.message);
@@ -17,9 +16,114 @@ const getUsers = (id) => {
             
             `)
             console.table(result);
+            console.log(`
+            
+            `)
             return result;
         }
     )
 };
 
-module.exports = getUsers;
+const getAllRole = () => {
+    db.query(
+        'SELECT r.title AS job_title, r.id AS role_id, d.dept_name AS department, r.salary FROM roles r LEFT JOIN department d ON r.department_id = d.id', 
+        function(err, result) {
+            if(err) {
+                console.log(err.message);
+                return;
+            }
+            console.log(`
+            
+            
+            
+            
+            `)
+            console.table(result);
+            console.log(`
+            
+            `)
+            return result;
+        }
+    )
+};
+
+const getAllDept = () => {
+    db.query(
+        'SELECT d.id AS department_id, d.dept_name AS department FROM department d', 
+        function(err, result) {
+            if(err) {
+                console.log(err.message);
+                return;
+            }
+            console.log(`
+            
+            
+            
+            
+            `)
+            console.table(result);
+            console.log(`
+            
+            `)
+            return result;
+        }
+    )
+};
+
+const mkDept = (dept_name) => {
+    db.query(
+        'INSERT INTO department (dept_name) VALUES (?)',
+        [dept_name],
+        function(err, result) {
+            if(err) {
+                console.log(err.message);
+                return;
+            }
+            return result.affectedRows;
+        }
+    )
+};
+
+const mkRole = (title, salary, dept_num) => {
+    db.query(
+        'INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)',
+        [title, salary, dept_num],
+        function(err, result) {
+            if(err) {
+                console.log(err.message);
+                return;
+            }
+            return result.affectedRows;
+        }
+    )
+};
+
+const addEmp = (first, last, role, manager) => {
+    db.query(
+        'INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+        [first, last, role, manager],
+        function(err, result) {
+            if(err) {
+                console.log(err.message);
+                return;
+            }
+            return result.affectedRows;
+        }
+    )
+};
+
+const updEmp = (name, role) => {
+    db.query(
+        'UPDATE employees SET role_id = ? WHERE id = ?',
+        [role, name],
+        function(err, result) {
+            if(err) {
+                console.log(err.message);
+                return;
+            }
+            return result.message;
+        }
+    )
+};
+
+module.exports = { getAllEmp, getAllRole, getAllDept, mkDept, mkRole, addEmp, updEmp };
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const getUsers = require('./models/queries');
+const queries = require('./models/queries');
 const inquirer = require('inquirer');
 
 console.log(`
@@ -7,24 +7,171 @@ console.log(`
 ==========================================================`);
 console.log('Printing first query.....');
 
-const userPrompts = () => {
+const empPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'first_name',
+            message: "What is the Employee's first name?"
+        },
+        {
+            type: 'input',
+            name: 'last_name',
+            message: "What is the Employee's last name?"
+        },
+        {
+            type: "number",
+            name: 'role_id',
+            message: 'What is their role?'
+        },
+        {
+            type: 'number',
+            name: 'manager_id',
+            message: "Who does this employee report to?"
+        }
+    ]).then(answer => {
+        //console.log(answer);
+        return queries.addEmp(answer.first_name, answer.last_name, answer.role_id, answer.manager_id);
+    }).catch(err => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+    })
+};
+
+const rolePrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'What is the role title?'
+        },
+        {
+            type: 'number',
+            name: 'salary',
+            message: 'What is the annual salary of this role?'
+        },
+        {
+            type: 'input',
+            name: 'dept_num',
+            message: 'What department is this role in?'
+        }
+    ]).then(answer => {
+        //console.log(answer);
+        return queries.mkRole(answer.title, answer.salary, answer.dept_num);
+    }).catch(err => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+    })
+};
+
+const deptPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'dept',
+            message: 'What is the name of the new department?'
+        }
+    ]).then(answer => {
+        //console.log(answer);
+        return queries.mkDept(answer.dept);
+    }).catch(err => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+    })
+};
+
+const updPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'number',
+            name: 'emp_num',
+            message: 'What is the ID number of the employee?'
+        },
+        {
+            type: 'number',
+            name: 'new_role',
+            message: 'What is their new role id number?'
+        }
+    ]).then(answer => {
+        //console.log(answer);
+        return queries.updEmp(answer.emp_num, answer.new_role);
+    }).catch(err => {
+        if(err) {
+            console.log(err);
+            return;
+        }
+    })
+};
+
+async function userPrompts() {
     return inquirer.prompt([
         {
             type: 'list',
             name: 'choice',
             message: 'What do you want to do?',
             choices: ['View All Employees', 'View All Roles', 'View All Departments', 'Create a Department', 'Create a Role', 'Add an Employee', 'Update Employee Role', 'Exit']
-        },
-        {
-            type: 'number',
-            name: 'id',
-            message: 'What is the Employee ID number?'
         }
     ])
-        .then(answers => {
-            console.log(answers);
-            getUsers(answers.id);
-            if(answers.choice !== 'Exit') {
+        .then( (answer) => {
+            //console.log(answer);
+            // if(answer.choice === 'View All Employees') {
+            //     console.log(queries.getAllEmp());
+            // }
+            // if(answer.choice === 'View All Roles') {
+            //     console.log(queries.getAllRole());
+            // }
+            // if(answer.choice === 'View All Departments') {
+            //     console.log(queries.getAllDept());
+            // }
+            // if(answer.choice === 'Create a Department') {
+            //     deptPrompt();
+            //     return;
+            // }
+            // if(answer.choice === 'Create a Role') {
+            //     rolePrompt();
+            //     return;
+            // }
+            // if(answer.choice === 'Add an Employee') {
+            //     empPrompt();
+            //     return;
+            // }
+            // if(answer.choice === 'Update Employee Role') {
+            //     updPrompt();
+            //     return userPrompts();
+            // }
+            // if(answer.choice !== 'Exit') {
+            //     return userPrompts();
+            // }
+            switch(answer.choice) {
+                case 'View All Employees':
+                    console.log(queries.getAllEmp());
+                    break;
+                case 'View All Roles':
+                    console.log(queries.getAllRole());
+                    break;
+                case 'View All Departments':
+                    console.log(queries.getAllDept());
+                    break;
+                case 'Create a Department':
+                    deptPrompt();
+                    break;
+                case 'Create a Role':
+                    rolePrompt();
+                    break;
+                case 'Add an Employee':
+                    empPrompt();
+                    break;
+                case 'Update Employee Role':
+                    updPrompt();
+                    break;
+            }
+            if(answer.choice !== 'Exit') {
                 return userPrompts();
             }
         })
